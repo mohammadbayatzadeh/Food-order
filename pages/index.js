5fbe15b382b96cf420b901d638e8e1bd97a84898
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-05-23 16:38:59 +0330

    config

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h2>Food Order</h2>
+      </main>
+    </>
+  );
+}
